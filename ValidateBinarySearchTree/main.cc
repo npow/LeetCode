382c379b78c7fae5fb238eb85115b0ccf31b1ca9
Nissan Pow <nissan.pow@gmail.com>
2012-10-21 01:20:59 -0400

    .

---
+++ ValidateBinarySearchTree/main.cc
@@ -0,0 +1,20 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+        return isValidBST(root, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+    }
+    bool isValidBST(TreeNode *root, int minVal, int maxVal) {
+        if (!root) return true;
+        if (root->val <= minVal || root->val >= maxVal) return false;
+        return isValidBST(root->left, minVal, root->val) && isValidBST(root->right, root->val, maxVal);
+    }
+};
