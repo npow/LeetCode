76eabb09047eede51cd4569b6729dae0487df9b6
Nissan Pow <nissan.pow@gmail.com>
2012-11-03 19:38:24 -0400

    .

---
+++ FirstMissingPositive/main.c
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        int i = 0;
+        while (i < n) {
+            while (i >= 0 && i < n && A[i] > 0 && A[i] <= n && A[i] != A[A[i]-1]) {
+                swap(A[i], A[A[i]-1]);
+            } 
+            ++i;
+        }
+        for (i = 0; i < n; ++i) {
+            if (A[i] != i+1) break;
+        }
+        return i+1;
+    }
+};
